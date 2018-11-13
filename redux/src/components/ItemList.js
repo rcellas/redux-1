c2c3e28888f4e409cd2c7cc218b02d22c4c21c6c
Unknown <rocio.cejudo.soronellas@gmail.com>
2018-11-13 17:51:34 +0100

    new itemlist

--- redux/src/components/ItemList.js
+++ redux/src/components/ItemList.js
@@ -2,33 +2,31 @@ import React, {Component} from 'react';
 
 class ItemList extends Component{
     constructor(){
-        super();
-
         this.state={
-            items:[
-                {
-                    id: 1,
-                    label: 'List item 1'
-                },
-
-                {
-                    id: 2,
-                    label: 'List item 2'
-                },
-
-                {
-                    id:3,
-                    label: 'List item 3'
-                },
-
-                {
-                    id: 4,
-                    label: 'List item 4'
-                }
-            ],
-            hasErrored:false,
-            isLoading:false
+            items:[],
         };
+    }
+
+    fetchData(url) {
+        this.setState({isLoading:true});
+
+        fetch(url)
+        .then((response)=>{
+            if(!response.ok){
+                throw Error(response.statusText);
+            }
+
+            this.setState({ isLoading: false});
+
+            return response;
+        })
+        .then((response)=> response.json())
+        .then((items)=> this.setState({items}))
+        .catch(()=> this.setState({hasErrored:true}));
+    }
+
+    componentDidMount(){
+        this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
     }
 
     render(){
